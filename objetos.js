41c4ea7129e595cd39cc17647dd8b0895494657c
Alan-838 <alanusmp1990@gmail.com>
2025-07-04 20:25:57 -0500

    Se agregaron varios ejemplos de object

---
+++ objetos.js
@@ -0,0 +1,85 @@
+// objeto: no es nada más que un elemento de nuestro entorno
+// por ejemplo si quisiera definir como objeto un carro 
+// objeto: un carro
+// key: value
+// marca: kia
+// puertas: 4
+// color: azul
+
+// declaramos un objeto
+
+let carro = { 
+    marca: "kia",
+    color: "azul",
+    electrico: true,
+    costo: 32.000
+}
+
+console.table(carro)
+
+// crear 3 objetos 1 persona, 1 electrodomestico con 5 propiedades como minimo cada uno
+
+let guilmoncha = { 
+    raza: "chusca",
+    color: "blanco",
+    cariñosa: true,
+    costo: 0,
+    mordelona: true
+}
+
+let mostacha = { 
+    raza: "persa",
+    color: "ploma",
+    cariñosa: true,
+    costo: 1000,
+    mordelona: false
+}
+
+let captain = { 
+    raza: "medio fino",
+    color: "gringo",
+    cariñoso: true,
+    costo: 500,
+    mordelon: true
+}
+
+let Carlitos = { 
+    carrera: "Fisico Nuclear",
+    pelo: "escazo",
+    estudioso: false,
+    sueldo: 1200,
+    trabajador: true
+}
+
+let Refrigeradora = { 
+    marca: "LG",
+    color: "plomo",
+    grande: false,
+    precio: 2500,
+    resistente: true
+}
+
+let celular = {
+    marca: "Xiaomi",
+    color: "negro",
+    precio: 1000,
+    pequeno: false,
+    resistente: true
+}
+
+let celular2 = {
+    marca: "Xiaomi",
+    color: "verde",
+    precio: 300,
+    resistente: false,
+    bonito: false,
+}
+
+let televisor = {
+    marca: "LG",
+    grande: true,
+    precio: 1000,
+    resistente: false,
+    pantalla: "LED"
+
+}
